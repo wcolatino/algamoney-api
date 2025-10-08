602437d0625d19dd2bd2ce72fdd07b495e801b6e
Willie Colatino <willie.colatino@eb.mil.br>
2025-10-08 16:53:38 -0300

    Inserção do jackson para deserialização, tratamento de falhas com ExceptionHandler

---
+++ src/main/java/com/colatinotech/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -0,0 +1,59 @@
+package com.colatinotech.algamoney.api.exceptionhandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+//CAPTURA EXCEÇÕES DA APLICAÇÃO
+@ControllerAdvice //Observa toda a aplicação
+public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		
+		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());//Passa o Locale atual
+		String mensagemDesenvolvedor = ex.getCause().toString();
+	
+		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+
+	
+	//Classe interna, apenas para tratamento de mensagens de usuário e desenvolvedor
+	public static class Erro {
+		
+		private String mensagemUsuario;
+		private String mensagemDesenvolvedor;
+		
+		public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+			this.mensagemUsuario = mensagemUsuario;
+			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+		}
+
+		public String getMensagemUsuario() {
+			return mensagemUsuario;
+		}
+
+		public String getMensagemDesenvolvedor() {
+			return mensagemDesenvolvedor;
+		}
+		
+		
+		
+		
+		
+	}
+
+}
